a8c435bc993976daee3eb3116120605af5cc15a6
sina <140723786+sinarahbari@users.noreply.github.com>
2025-04-03 20:02:42 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Columbus Cafe");
+
+document.addEventListener("scroll", function () {
+  const footer = document.querySelector(".footer");
+  const footerPosition = footer.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+
+  if (footerPosition < screenHeight) {
+    footer.classList.add("visible");
+  }
+});
